fcde5584912cb30218aadedc3b257027cf217d89
chacha <15073640+cclecle@users.noreply.github.com>
2025-03-21 23:12:26 +0100

    start IBM PC300 6275 support

---
+++ src/mainboard/ibm/PC300_6275/bootblock.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <bootblock_common.h>
+#include <superio/smsc/smscsuperio/smscsuperio.h>
+
+
+#define SERIAL_DEV PNP_DEV(0x3f0, W83977TF_SP1)
+
+void bootblock_mainboard_early_init(void)
+{
+	smscsuperio_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
+}
