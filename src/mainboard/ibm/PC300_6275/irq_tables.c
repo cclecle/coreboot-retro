a2c6a5963e0f2075dce02687b556325f58bfa9e0
chacha <15073640+cclecle@users.noreply.github.com>
2025-03-24 08:57:03 +0100

    fix

--- src/mainboard/ibm/PC300_6275/irq_tables.c
+++ src/mainboard/ibm/PC300_6275/irq_tables.c
@@ -85,30 +85,3 @@ printk(BIOS_INFO, "%s DONE.\n", __func__);
 
 return (unsigned long)pirq_info;
 }
-
-
-static const struct irq_routing_table intel_irq_routing_table = {
-	PIRQ_SIGNATURE,
-	PIRQ_VERSION,
-	32 + 16 * CONFIG_IRQ_SLOT_COUNT,/* Max. number of devices on the bus */
-	0x00,			/* Interrupt router bus */
-	(0x04 << 3) | 0x0,	/* Interrupt router device */
-	0,			/* IRQs devoted exclusively to PCI usage */
-	0x8086,			/* Vendor */
-	0x122e,			/* Device */
-	0,			/* Miniport data */
-	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, /* u8 rfu[11] */
-	0x54,			/* Checksum */
-	/* clang-format off */
-	{
-		/* bus,       dev|fn,   {link, bitmap}, {link, bitmap}, {link, bitmap}, {link, bitmap},  slot, rfu */
-		{0x00,(0x02 << 3)|0x0, {{0x60, 0x1eb8}, {0x61, 0x1eb8}, {0x62, 0x1eb8}, {0x63, 0x01eb8}}, 0x0, 0x0},
-		{0x00,(0x01 << 3)|0x0, {{0x60, 0x1eb8}, {0x61, 0x1eb8}, {0x62, 0x1eb8}, {0x63, 0x01eb8}}, 0x0, 0x0},
-	}
-	/* clang-format on */
-};
-
-unsigned long write_pirq_routing_table(unsigned long addr)
-{
-	return copy_pirq_routing_table(addr, &intel_irq_routing_table);
-}
