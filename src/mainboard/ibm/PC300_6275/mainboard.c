3318646170dc24d0379fd499533f932255b73cdd
chacha <15073640+cclecle@users.noreply.github.com>
2025-04-21 17:44:33 +0200

    add sio smi config for keyboard and mouse

--- src/mainboard/ibm/PC300_6275/mainboard.c
+++ src/mainboard/ibm/PC300_6275/mainboard.c
@@ -2,6 +2,7 @@
 
 #include <device/pci_ops.h>
 #include <southbridge/intel/i82371eb/i82371eb.h>
+#include <console/console.h>
 
 /**
  * Mainboard specific enables.
@@ -12,6 +13,30 @@
 
 static void mainboard_init(void *chip_info)
 {
+	u8 reg8;
+	printk(BIOS_INFO, "300GL mainboard init\n");
+
+	outb(0x55, 0x370); // enter SIO config mode
+
+	outb(0x08, 0x370 + 0x07); // access to logical device 8 (Aux config)
+
+	reg8 = inb(0x370 + 0xb4); // read SMI Enable Register 1
+	printk(BIOS_INFO, "SMI Enable Register 1: 0x%x\n",reg8);
+
+	reg8 = inb(0x370 + 0xb5); // read SMI Enable Register 2
+	printk(BIOS_INFO, "SMI Enable Register 2: 0x%x\n",reg8);
+
+	// Enable mouse & Kb SMI:
+	reg8 |= (1 << 1) | (1 << 0);
+	// Enable nSMI Interrupt onto Serial IRQ
+	reg8 |= (1 << 6);
+	outb(reg8, 0x370 + 0xb5); // read SMI Enable Register 2
+
+	reg8 = inb(0x370 + 0xc0); // read Pin Multiplex Controls
+	printk(BIOS_INFO, "Pin Multiplex Controls: 0x%x\n",reg8);
+
+	outb(0xaa, 0x370); // exit SIO config mode
+
 }
 
 
